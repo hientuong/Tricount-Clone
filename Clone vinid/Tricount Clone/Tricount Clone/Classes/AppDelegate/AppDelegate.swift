32345cc994e89caf2f286791bf4d85a9c382b1b3
Tuong Nguyen <hientuong.hust@gmail.com>
2019-03-21 10:45:16 +0700

    initial project

---
+++ Clone vinid/Tricount Clone/Tricount Clone/Classes/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Tricount Clone
+//
+//  Created by Hien Tuong on 3/21/19.
+//  Copyright © 2019 Hien Tuong. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ Clone vinid/Tricount Clone/Tricount Clone/Classes/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Tricount Clone
+//
+//  Created by Hien Tuong on 3/21/19.
+//  Copyright © 2019 Hien Tuong. All rights reserved.
+//
+
+import UIKit
+import VGBase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
